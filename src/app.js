451a76cef7b810c427578d27911a6e8d83458ed9
tarikochuery <ttarikechuery@gmail.com>
2023-01-21 21:06:43 -0300

    Adjust for deploy

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import authRoute from './routes/authRoutes.js';
 import transactionRoute from './routes/transactionRoutes.js';
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(json());
